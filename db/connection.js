8416f4f198443666b32a259b6d0459ee710501a3
Adrian Szeto <adrianduongszeto@gmail.com>
2022-08-05 10:26:18 -0700

    updated server

--- db/connection.js
+++ db/connection.js
@@ -3,13 +3,9 @@ const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'root',
-    password: '',
+    user: 'db',
+    password: 'passpass',
     database: 'employee_db'
-});
-
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
 });
 
 module.exports = db;
