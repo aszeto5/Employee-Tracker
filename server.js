ef6251841526fbfafee426af6f734a523b749a06
Adrian Szeto <adrianduongszeto@gmail.com>
2022-08-11 12:48:52 -0700

    password update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
     user: 'root',
 
     // Your password
-    password: 'Asupreme13',
+    password: 'enter mysql password here',
     database: 'employeesDB'
 });
 
